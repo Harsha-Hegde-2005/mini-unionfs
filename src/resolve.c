c59f71ab09602a50e272f57d0f3bd771c6e49f0f
HARSHAVARDHAN N <pes2ug23cs214@pesu.pes.edu>
2026-03-27 22:23:31 +0530

    feat: add resolve.c skeleton with includes and function signature

---
+++ src/resolve.c
@@ -0,0 +1,11 @@
+#include "fs.h"
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+int resolve_path(const char *path, char *resolved_path) {
+    // TODO: implement path resolution
+    return -ENOENT;
+}

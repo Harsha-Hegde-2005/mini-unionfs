2fd74e50de14943abaaaa840c5147b0e397c7d61
HARSHAVARDHAN N <pes2ug23cs214@pesu.pes.edu>
2026-03-31 22:46:18 +0530

    feat: implement unionfs_read resolving file from upper or lower layer

--- src/file_ops.c
+++ src/file_ops.c
@@ -3,11 +3,35 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-
+#include <limits.h>
 
 int unionfs_read(const char *path, char *buf, size_t size, off_t offset,
                  struct fuse_file_info *fi) {
-    return -ENOENT;
+    char resolved_path[PATH_MAX];
+    int fd;
+    int res;
+
+    // Resolve path (upper or lower)
+    if (resolve_path(path, resolved_path) != 0) {
+        return -ENOENT;
+    }
+
+    // Open file (read-only)
+    fd = open(resolved_path, O_RDONLY);
+    if (fd == -1) {
+        return -errno;
+    }
+
+    // Read using pread
+    res = pread(fd, buf, size, offset);
+    if (res == -1) {
+        res = -errno;
+    }
+
+    // Close file
+    close(fd);
+
+    return res;
 }
 
 
